3f5e2933f6d73098df943a1935612a7c41a8ee9b
cjariaga <cjariaga@aggies.ncat.edu>
2026-02-09 09:53:43 -0500

    First commit
    Add initial game setup with required variables

---
+++ Clayan_Ariaga_assignment_4.py
@@ -0,0 +1,21 @@
+"""
+COMP 163 - Introduction to Programming
+Assignment: Chapter 4 - College Life Adventure Game
+Name: Clayan junior Ariaga
+GitHub Username: cjariaga
+Date: [Submission Date]
+Description: [2-3 sentences describing your game's theme and objective]
+AI Usage: [Describe any AI assistance OR write "None"]
+"""
+
+#Required Variables (use these exact names):
+student_name = "Clayan Ariaga"  # Replace with your actual name
+current_gpa = float(input("current gpa between 1.0-4.0: "))# Float between 1.0-4.0
+study_hours = int(input("Study hours:"))# Integer (e.g., 25)
+social_points = int(input("social points"))# Integer (e.g., 50)
+stress_level = int(input("Stress level 0-100:"))# Integer 0-100
+print(f"Welcome {student_name} to the College Life Game")
+print("Here are your starting stats:")
+print(f"GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
+
+
